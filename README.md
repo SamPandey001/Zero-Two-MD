a4687c6d9bea021d835ce471f7e334c6c87fed48
Shubha Pratim Biswas <104565822+SamPandey001@users.noreply.github.com>
2022-06-07 23:15:22 +0530

    Zero Two

---
+++ README.md
@@ -0,0 +1,86 @@
+<h1 align="center">🍭 𝗭𝗲𝗿𝗼 𝗧𝘄𝗼 𝗠𝗗 🍭<br></h1>
+<p align="center">
+  <img src="https://wallpapercave.com/uwp/uwp2350296.jpeg" alt="21-akeno-wallpaper-Images-of-Akeno-Himejima-Wallpaper-industrious-info" />
+</p>
+
+<p align="center"> 
+  A Moduler WhatsApp Bot designed for both PM and Groups - To take your boring WhatsApp usage into a whole different level.
+
+<p align="center">
+  <a href="https://github.com/fantox001/ZERO-TWO-MD/fork">
+    <img src="https://img.shields.io/github/forks/fantox001/ZERO-TWO-MD?label=Fork&style=social">
+    
+    
+  <a href="https://github.com/fantox001/Zero-Two-Md/stargazers">
+    <img src="https://img.shields.io/github/stars/fantox001/Zero-Two-Md/?style=social">
+  </a>
+
+<p align="center">
+  <a href="httsp://github.com/fantox001/Zero-Two-Md">
+    <img src="https://img.shields.io/github/repo-size/phaticusthiccy/Fantox001/Zero-Two-Md?color=purple&label=Repo%20Size&style=plastic <p align="center">
+
+## Thanks for Everything 
+- [XEON](https://github.com/dgxeon)
+
+![Total Repo views](https://gpvc.arturio.dev/sampandey001)
+
+
+ ### Simple Method 
+
+[![Run on Repl.it](https://repl.it/badge/github/fantox001/ZERO-TWO-MD)](https://repl.it/@sampandey)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/fantox001/Zero-Two-Md)
+
+## Whatsapp Group
+
+<a href="https://chat.whatsapp.com/KK6AVKEwPVJ0aXoWo2cK2g"><img src="https://img.shields.io/badge/Join Group-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+
+
+### Guide
+> [Zero Two Wiki (Full Guide)](https://github.com/fantox001/Zero-Two-Md/wiki) 
+
+### The Hard Method (Not Recommend)
+```js
+apt update
+apt upgrade
+pkg update && pkg upgrade
+pkg install bash
+pkg install libwebp
+pkg install git -y
+pkg install nodejs -y 
+pkg install ffmpeg -y 
+pkg install wget
+pkg install imagemagick -y
+git clone https://github.com/fantox001/Zero-Two-Md
+cd Zero-Two-Md
+npm start
+```
+
+### ⚠️ Warning! 
+```
+ This bot is not a official bot by Whatsapp INC So if you want to use it then feel free to do so.
+ Your WhatsApp Number can be banned by Whatsapp as it is not official
+ Zero-Two-MD bot is still in the development stage, so there can be few bugs.Feel free to report that to us.
+```
+
+## Developers
+
+[![XEON](https://github.com/dgxeon.png?size=100)](https://github.com/dgxeon) | [![Fantox](https://github.com/fantox001.png?size=100)](https://github.com/fantox001) | [![Sam Pandey](https://github.com/sampandey001.png?size=109)](https://github.com/sampandey001) | [![ISSA](https://github.com/issa2001.png?size=80)](https://github.com/issa2001) | [![Aaditya](https://github.com/addycracker.png?size=80)](https://github.com/addycracker)
+----|----|----|----|----
+[XEON](https://wa.me/916909137213) | [Fantox](https://wa.me/918101187835) | [Sam Pandey](https://wa.me/919628516236) | [Issa](https://wa.me/254115175696) | [Aditya](https://wa.me/919681777261)
+Base Bot  | Developer, Bug Fixes, Modules |Developer,Bug Fixes,Base, Modules, | Modules, Idea | Modules
+
+
+
+## License
+This project is protected by `GNU General Public Licence v3.0` license.
+
+## Contributing
+Feel free to contribute.
+
+### Disclaimer
+This is an open source WhatsApp bot based on Multi Device created by [`@fantox`](https://github.com/FantoX001) & `Zero Two Inc` working with [`@adiwajshing/baileys`](https://github.com/adiwajshing/baileys). Do not recode it without notifying the owners otherwise you will be petitioned for fraud & copyright infringement!
+
+## Help
+Give a ⭐ to this repo if it helped you.
---
+++ config.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+var _0x39a629=_0x3707;(function(_0x3558fb,_0x2f8f90){var _0x560da3=_0x3707,_0x317876=_0x3558fb();while(!![]){try{var _0x4f6c62=parseInt(_0x560da3(0x128))/0x1+-parseInt(_0x560da3(0x127))/0x2+-parseInt(_0x560da3(0x125))/0x3*(-parseInt(_0x560da3(0x11f))/0x4)+-parseInt(_0x560da3(0x122))/0x5+-parseInt(_0x560da3(0x11d))/0x6*(-parseInt(_0x560da3(0x123))/0x7)+-parseInt(_0x560da3(0x11c))/0x8*(-parseInt(_0x560da3(0x129))/0x9)+-parseInt(_0x560da3(0x120))/0xa;if(_0x4f6c62===_0x2f8f90)break;else _0x317876['push'](_0x317876['shift']());}catch(_0x5ea152){_0x317876['push'](_0x317876['shift']());}}}(_0x5018,0x41138),global[_0x39a629(0x121)]=fs[_0x39a629(0x126)]('./assets/img/zt.jpg'),global[_0x39a629(0x11e)]=fs['readFileSync']('./assets/img/zt.jpg'),global['err4r']=fs[_0x39a629(0x126)](_0x39a629(0x124)),global['thumb']=fs['readFileSync']('./assets/img/zt.jpg'));function _0x3707(_0xe7bf43,_0x38bb66){var _0x50188a=_0x5018();return _0x3707=function(_0x370761,_0x5873a5){_0x370761=_0x370761-0x11c;var _0x47cced=_0x50188a[_0x370761];return _0x47cced;},_0x3707(_0xe7bf43,_0x38bb66);}function _0x5018(){var _0x53ef84=['557150DrhUWW','thum','2522310pQQOAL','14SVGkFl','./assets/img/zt.jpg','78XxhuEI','readFileSync','367238hsnNmA','346789RREzXR','103167HTlrJg','296PnOiar','184218pNjiMq','log0','27388eSKveb'];_0x5018=function(){return _0x53ef84;};return _0x5018();} 
+global.ytname = "YT: Fantox Gaming"
+global.location = "India, West-Bengal, Kolkata"
+global.botscript = "https://github.com/Fantox001/Zero-Two-Md"
+global.antitags = true
+global.rkyt = []
+global.banUser = []
+global.banchat = []
+global.ntilink = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.wlcm = []
+global.gcrevoke = []
+var _0x1a32e8=_0x41cc;function _0x41cc(_0x40a8d6,_0x3701e2){var _0x2635b7=_0x2635();return _0x41cc=function(_0x41cc3a,_0x231162){_0x41cc3a=_0x41cc3a-0x1ee;var _0x1a7446=_0x2635b7[_0x41cc3a];return _0x1a7446;},_0x41cc(_0x40a8d6,_0x3701e2);}(function(_0x1ecbf2,_0x7bdf04){var _0x5aa09a=_0x41cc,_0x3df619=_0x1ecbf2();while(!![]){try{var _0x5cc6d3=-parseInt(_0x5aa09a(0x1f7))/0x1*(-parseInt(_0x5aa09a(0x208))/0x2)+-parseInt(_0x5aa09a(0x20a))/0x3+parseInt(_0x5aa09a(0x1fe))/0x4+-parseInt(_0x5aa09a(0x213))/0x5*(parseInt(_0x5aa09a(0x214))/0x6)+-parseInt(_0x5aa09a(0x1ef))/0x7+parseInt(_0x5aa09a(0x1f4))/0x8*(-parseInt(_0x5aa09a(0x1f1))/0x9)+-parseInt(_0x5aa09a(0x207))/0xa*(-parseInt(_0x5aa09a(0x211))/0xb);if(_0x5cc6d3===_0x7bdf04)break;else _0x3df619['push'](_0x3df619['shift']());}catch(_0x2d5d82){_0x3df619['push'](_0x3df619['shift']());}}}(_0x2635,0xb11de),global[_0x1a32e8(0x203)]={'zenz':_0x1a32e8(0x1f3),'amel':_0x1a32e8(0x1fa),'bx':_0x1a32e8(0x201),'hardianto':_0x1a32e8(0x20e),'jonaz':_0x1a32e8(0x1fc),'neoxr':_0x1a32e8(0x212),'xteam':_0x1a32e8(0x1ee),'nzcha':_0x1a32e8(0x206),'bg':_0x1a32e8(0x204),'fdci':_0x1a32e8(0x1f6),'rey':'https://server-api-rey.herokuapp.com','dzx':_0x1a32e8(0x1f9),'bsbt':_0x1a32e8(0x1fb),'zahir':_0x1a32e8(0x1f5),'zeks':_0x1a32e8(0x210),'zekais':_0x1a32e8(0x1f2),'hardianto':_0x1a32e8(0x202),'pencarikode':_0x1a32e8(0x20f),'erdwepe':_0x1a32e8(0x209),'lolhuman':'https://api.lolhuman.xyz','LeysCoder':_0x1a32e8(0x200)},global[_0x1a32e8(0x1ff)]={'https://zenzapis.xyz':_0x1a32e8(0x20d),'https://melcanz.com':_0x1a32e8(0x205),'https://hardianto.xyz':'hardianto','https://api.xteam.xyz':'Dawnfrostkey','https://zahirr-web.herokuapp.com':_0x1a32e8(0x20b),'https://bsbt-api-rest.herokuapp.com':'benniismael','https://server-api-rey.herokuapp.com':_0x1a32e8(0x20c),'https://api.zeks.xyz':'apivinz','https://hardianto-chan.herokuapp.com':'hardianto','https://pencarikode.xyz':_0x1a32e8(0x1f0),'https://leyscoders-api.herokuapp.com':_0x1a32e8(0x1fd),'https://zekais-api.herokuapp.com':_0x1a32e8(0x1f8),'https://api.lolhuman.xyz':'pelitbetsihluwh'});function _0x2635(){var _0x254d9e=['APIKeys','https://leyscoders-api.herokuapp.com','https://bx-hunter.herokuapp.com','https://hardianto-chan.herokuapp.com','APIs','http://bochil.ddns.net','elaina','http://nzcha-apii.herokuapp.com','23240MWEtdw','2mgCnbp','https://erdwpe-api.herokuapp.com','4057560PxhLYH','zahirgans','apirey','4ad2c1556c','https://hardianto.xyz','https://pencarikode.xyz','https://api.zeks.xyz','12881pMnlwi','https://neoxr-api.herokuapp.com','5455EMlIaQ','618qCuUOC','https://api.xteam.xyz','6307182QLPFhR','pais','5333697XNqdoy','http://zekais-api.herokuapp.com','https://zenzapis.xyz','16SRFgTg','https://zahirr-web.herokuapp.com','https://api.fdci.se','544763FimhMg','zekais','https://api.dhamzxploit.my.id','https://melcanz.com','https://bsbt-api-rest.herokuapp.com','https://jonaz-api-v2.herokuapp.com','dappakntlll','4041952CDMDxA'];_0x2635=function(){return _0x254d9e;};return _0x2635();}
+//═══════[modification]════════\\
+global.owner = ['919628516236','916307151530','918953092716' , '919330953026' , '918101187835'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['919628516236'] //another owner number
+global.premium = ['919628516236'] //premium number
+global.pengguna = 'Zero-Two' //username
+global.botnma = 'Zero-Two' //bot name
+global.watermark = "ENJOY WITH ZERO TWO"
+global.dogeemoji = "✨"
+global.botname = 'Zero-Two'
+global.ownernma = 'Team Zero Two' //owner name
+global.ownername = 'Zero Two' //owner name
+global.packname = 'Zero-Two ' //sticker package name
+global.author = 'Dont Steal' //sticker author name
+global.sessionName = 'session.sam' //session name
+global.linkgrupss = "https://instagam.com/ikshwaku.pandey" //ur gc link
+global.linkgrupss2 = "https://instagram.com/ikshwaku.pandey" //ur gc link2
+global.websitex = "https://instagram.com/ikshwaku.pandey" //ur website
+global.prefa = ['-'] //prefix
+global.sp = '✨' //design
+global.mess = {
+    success: 'Done ✓',
+    admin: '𝑻𝒉𝒊𝒔 𝑪𝒐𝒎𝒎𝒂𝒏𝒅 𝒊𝒔 𝒐𝒏𝒍𝒚 𝒇𝒐𝒓 𝑮𝒓𝒐𝒖𝒑 𝑨𝒅𝒎𝒊𝒏  ',
+    botAdmin: '𝑰𝒇 𝒚𝒐𝒖 𝒓𝒆𝒂𝒍𝒍𝒚 𝒘𝒂𝒏𝒕 𝒎𝒆 𝒕𝒐 𝒅𝒐 𝒕𝒉𝒂𝒕,𝑮𝒊𝒗𝒆 𝒎𝒆 𝒂𝒅𝒎𝒊𝒏 𝒑𝒓𝒊𝒗𝒊𝒍𝒆𝒈𝒆❗',
+    owner: '𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐢𝐬 𝐨𝐧𝐥𝐲 𝐟𝐨𝐫 𝐌𝐲 𝐋𝐨𝐫𝐝 𝐎𝐰𝐧𝐞𝐫 ' ,
+    group: '𝑻𝒉𝒊𝒔 𝑭𝒆𝒂𝒕𝒖𝒓𝒆 𝒊𝒔 𝑶𝒏𝒍𝒚 𝑭𝒐𝒓 𝑮𝒓𝒐𝒖𝒑𝒔! ',
+    private: '𝚃𝚑𝚒𝚜 𝙵𝚎𝚊𝚝𝚞𝚛𝚎 𝚒𝚜 𝙾𝚗𝚕𝚢 𝙵𝚘𝚛 𝙿𝚛𝚒𝚟𝚊𝚝𝚎 𝙲𝚑𝚊𝚝!',
+    bot: '𝑻𝒉𝒊𝒔 𝒇𝒆𝒂𝒕𝒖𝒓𝒆 𝒊𝒏 𝒐𝒏𝒍𝒚 𝒇𝒐𝒓 𝒕𝒉𝒆 𝒃𝒐𝒕 𝒏𝒖𝒎𝒃𝒆𝒓',
+    wait: '𝑷𝒓𝒐𝒄𝒆𝒔𝒔𝒊𝒏𝒈 𝒚𝒐𝒖𝒓 𝒓𝒆𝒒𝒖𝒆𝒔𝒕',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    linkm: 'Where is the link dude?',
+    ban: 'You are banned by the owner, if you want to be unbanned, chat with owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+global.limitawal = {
+    premium: "Infinity", //premium user limit
+    free: 40 //free user limit
+}
+function _0x541c(){var _0x520861=['998048IAMZZZ','./assets/img/zt.jpg','thumb','777904mDNRjN','676040CCHFKt','readFileSync','967962kDYBDh','2833800DPpmXk','1317540Ofobcc','1314yvlPXk','2933gQtPbt'];_0x541c=function(){return _0x520861;};return _0x541c();}function _0x58cd(_0x281a3c,_0x3b6c20){var _0x541cd5=_0x541c();return _0x58cd=function(_0x58cd30,_0x40480a){_0x58cd30=_0x58cd30-0xaf;var _0x2d9baf=_0x541cd5[_0x58cd30];return _0x2d9baf;},_0x58cd(_0x281a3c,_0x3b6c20);}var _0x325c36=_0x58cd;(function(_0x2da43c,_0x5f3f1a){var _0x4e00f1=_0x58cd,_0x4299cb=_0x2da43c();while(!![]){try{var _0x15c880=parseInt(_0x4e00f1(0xb7))/0x1+-parseInt(_0x4e00f1(0xb4))/0x2+-parseInt(_0x4e00f1(0xaf))/0x3+parseInt(_0x4e00f1(0xb0))/0x4+-parseInt(_0x4e00f1(0xb1))/0x5+-parseInt(_0x4e00f1(0xb2))/0x6*(-parseInt(_0x4e00f1(0xb3))/0x7)+-parseInt(_0x4e00f1(0xb8))/0x8;if(_0x15c880===_0x5f3f1a)break;else _0x4299cb['push'](_0x4299cb['shift']());}catch(_0x4ed7c3){_0x4299cb['push'](_0x4299cb['shift']());}}}(_0x541c,0x63b68),global[_0x325c36(0xb6)]=fs[_0x325c36(0xb9)](_0x325c36(0xb5)));
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+	require(file)
+})
